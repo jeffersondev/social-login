55ea94b66c1ba2858a570917ed444b253507a9b8
Jefferson Martins de Andrade <jeffersonmandrade@gmail.com>
2018-05-23 21:19:12 -0300

    improve structure

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+const $ = document.querySelector.bind(document)
+
 function onSignIn(googleUser) {
-  const profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  renderUserProfile(googleUser)
 }
 
 function signOut() {
-  const auth2 = gapi.auth2.getAuthInstance();
+  const auth2 = gapi.auth2.getAuthInstance()
   auth2.signOut().then(function () {
-    console.log('User signed out.');
-  });
+    renderUserProfile()
+  })
 }
+
+function renderUserProfile(googleUser) {
+  console.log(googleUser)
+  if (googleUser) {
+    const profile = googleUser.getBasicProfile()
+    console.log('ID: ' + profile.getId())
+    console.log('Full Name: ' + profile.getName())
+    console.log('Given Name: ' + profile.getGivenName())
+    console.log('Family Name: ' + profile.getFamilyName())
+    console.log('Image URL: ' + profile.getImageUrl())
+    console.log('Email: ' + profile.getEmail())
+  } else {
+    console.log('User signed out.')
+  }
+}
+
+function initialize() {
+  if (auth2.isSignedIn.get()) {
+    renderUserProfile(auth2.currentUser.get())
+  }
+}
+
+initialize()
